eace40c1bed0c6534682239897133731e262638d
Mitchell <mbruckner807@gmail.com>
2013-08-22 14:35:51 -0500

    removed old logger registration functions

--- server.py
+++ server.py
@@ -4,8 +4,6 @@ from functools import wraps
 from flask import Flask, request, g, make_response, send_file, request, Response
 from db_handler import db_handler
 
-import server_state
-
 DATABASE = '/tmp/data.db'
 
 app = Flask(__name__)
@@ -60,27 +58,6 @@ def handle(projectname):
                 vals[check]=request.args[check]
         return g.db.get_data(projectname,**vals)
     return ":)"
-
-@app.route('/projects/<projectname>/addlogger/')
-def add_logger(projectname):
-    l=server_state.loggeraccept("loggerconfig.json")
-    if l.addlogger() :
-        l.write("loggerconfig.json")
-        return str(g.db.add_logger(projectname))
-    else :
-        return "NO SOUP FOR YOU!"
-
-@app.route('/register/')
-def register():
-    l=server_state.loggeraccept("loggerconfig.json")
-    if "usetime" in request.args :
-        l.usetime=bool(request.args["usetime"])
-    if "endtime" in request.args :
-        l.endtime=float(request.args["endtime"])
-    if "numleft" in request.args :
-        l.numleft=int(request.args["numleft"])
-    l.write("loggerconfig.json")
-    return json.dumps({"usetime":l.usetime,"endtime":l.endtime,"numleft":l.numleft})
 
 @app.route('/loggers/', methods=['GET'])
 def getloggers():
